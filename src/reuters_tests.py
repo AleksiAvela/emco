461ac7cb0752cc0d0b8e747b663ce3fdba3ece36
AleksiAvela <106800331+AleksiAvela@users.noreply.github.com>
2025-01-14 14:28:42 +0200

    Update reuters_tests.py

--- src/reuters_tests.py
+++ src/reuters_tests.py
@@ -15,6 +15,7 @@ from imblearn.over_sampling import SMOTE
 from imblearn.over_sampling import ADASYN
 from pydro.src.dro import DistributionalRandomOversampling
 from DECOMlike import LDAOS
+from edaos import EDAOversampling as EDAOS
 from emco import ExtrapolatedMarkovChainOversampling as EMCO
 
 
@@ -44,7 +45,7 @@ svc_tol = 1e-3
 
 ### Only classes with minority frequency lower than 0.75*balance_ratio are selected
 balance_ratio  = 0.2   # Relative minority frequency after sampling
-only_headlines = False # True -> only headlines, False -> full Reuters articles
+only_headlines = False  # True -> only headlines, False -> full Reuters articles
 train_min_df   = 2     # Only words that appear more than "train_min_df" times in
 		       # training documents are included in vocabulary
 
@@ -67,6 +68,30 @@ category_averages    = {}
 
 start_time = datetime.now()
 
+### Initialize the training and test sets:
+tr_raw, tr_docs, tr_vocabulary, tr_labels, te_docs, te_labels = initialize_data(
+	   stopwords=stopwords,
+	   train_min_df=train_min_df,
+	   only_headlines=only_headlines)
+
+tr_corpus = [' '.join(doc) for doc in tr_docs]
+te_corpus = [' '.join(doc) for doc in te_docs]
+
+# tf-idf transformer with the default settings:
+pipe = Pipeline([('count', CountVectorizer(vocabulary=tr_vocabulary)),
+		 ('tfidf', TfidfTransformer(norm=norm,
+					    use_idf=idf,
+					    smooth_idf=smooth, 
+					    sublinear_tf=tf))]).fit(tr_corpus)
+
+X_tr = dok_matrix(pipe.transform(tr_corpus))
+X_te = dok_matrix(pipe.transform(te_corpus))
+
+### Free some memory:
+te_docs   = []
+te_corpus = []
+
+
 for j, category in enumerate(categories):
 	
 	print(f"\t * Category {j+1} / {len(categories)} * ")
@@ -79,32 +104,18 @@ for j, category in enumerate(categories):
 		
 		if k == 0:
 			
-			### Initialize the training and test sets:
-			tr_docs, tr_vocabulary, y_tr, te_docs, y_te = initialize_data(
-				   pos_class=category,
-				   stopwords=stopwords,
-				   train_min_df=train_min_df,
-				   only_headlines=only_headlines)
+			### Create binary target vectors from the raw labels:
+			y_tr, y_te = [], []
+			for train_label_set in tr_labels:
+				y_tr.append(int(category in train_label_set))
+			for test_label_set in te_labels:
+				y_te.append(int(category in test_label_set))
 			
 			### Save the category frequencies:
 			category_frequencies[category] = sum(y_tr) / len(y_tr)
 			
-			tr_corpus = [' '.join(doc) for doc in tr_docs]
-			te_corpus = [' '.join(doc) for doc in te_docs]
-			
-			# tf-idf transformer with the default settings:
-			pipe = Pipeline([('count', CountVectorizer(vocabulary=tr_vocabulary)),
-					 ('tfidf', TfidfTransformer(norm=norm,
-								    use_idf=idf,
-								    smooth_idf=smooth, 
-								    sublinear_tf=tf))]).fit(tr_corpus)
-			
-			X_tr = dok_matrix(pipe.transform(tr_corpus))
-			X_te = dok_matrix(pipe.transform(te_corpus))
-			
+			### Initialize the oversampling methods:
 			sampling_strategy = balance_ratio/(1-balance_ratio)
-			
-			### Initialize the oversampling methods: 
 			ros = ROS(sampling_strategy=sampling_strategy)
 			### Number of nearest neighbors for SMOTE and ADASYN:
 			n_neighbors = min(5, sum(y_tr)-1) # max neighbors = 5 (the default value)
@@ -122,16 +133,17 @@ for j, category in enumerate(categories):
 			test_nwords  = np.asarray(X_te.sum(axis=1)).reshape(-1)
 			# These are the original DECOM hyperparameters:
 			decom = LDAOS(n_topics=30, alpha=50/30, beta=0.01, max_iter=300)
+			# EDA Oversampling:
+			edaos = EDAOS(s_ratio=balance_ratio)
+			# EMCO:
 			mco   = EMCO(gamma=0)
 			emco  = EMCO(gamma=1)
+			emco2 = EMCO(gamma=0.1)
 			### Fitting EMCO doesn't include randomness, so do it only
 			### once for each category:
 			mco.fit(tr_docs, y_tr, pos_class=1)
 			emco.fit(tr_docs, y_tr, pos_class=1)
-			### Free some memory:
-			tr_docs   = []
-			te_docs   = []
-			te_corpus = []
+			emco2.fit(tr_docs, y_tr, pos_class=1)
 		
 		### ROS:
 		Xros, yros = ros.fit_resample(X_tr, y_tr)
@@ -157,26 +169,37 @@ for j, category in enumerate(categories):
 		# Then the whole DECOM sample is transformed:
 		Xdecom = pipe['tfidf'].transform(dok_matrix(Xdecom))
 		
+		### EDA Oversampling:
+		eda_docs, yeda = edaos.sample(tr_raw, y_tr)
+		Xeda = dok_matrix(pipe.transform(eda_docs))
+		
 		### MCO and EMCO:
 		# First oversample the synthetic documents:
-		mcodocs, ymco   = mco.sample(s_ratio=balance_ratio, length='auto', complete=True)
-		emcodocs, yemco = emco.sample(s_ratio=balance_ratio, length='auto', complete=True)
+		mcodocs, ymco     = mco.sample(s_ratio=balance_ratio, length='auto', complete=True)
+		emcodocs, yemco   = emco.sample(s_ratio=balance_ratio, length='auto', complete=True)
+		emcodocs2, yemco2 = emco2.sample(s_ratio=balance_ratio, length='auto', complete=True)
 		# Then transform the oversampled data:
-		Xmco  = dok_matrix(pipe.transform([' '.join(doc) for doc in mcodocs]))
-		Xemco = dok_matrix(pipe.transform([' '.join(doc) for doc in emcodocs]))
+		Xmco   = dok_matrix(pipe.transform([' '.join(doc) for doc in mcodocs]))
+		Xemco  = dok_matrix(pipe.transform([' '.join(doc) for doc in emcodocs]))
+		Xemco2 = dok_matrix(pipe.transform([' '.join(doc) for doc in emcodocs2]))
 		
 		print("\nTesting with linear SVM ...")
 		print(datetime.now(), "\n")
 		
 		method,bAcc,TPR,TNR,prec,f_1,f_2 = [],[],[],[],[],[],[]
-		for X, y, name in zip([X_tr, Xros, Xsmote, Xada, Xdro, Xdecom, Xemco, Xmco],
-				      [y_tr, yros, ysmote, yada, ydro, ydecom, yemco, ymco],
-				      ["Original","ROS","SMOTE","ADASYN","DRO","DECOM","EMCO","MCO"]):
+		for X, y, name in zip(
+				[X_tr, Xros, Xsmote, Xada, Xdro,
+				 Xdecom, Xeda, Xemco, Xemco2, Xmco],
+				[y_tr, yros, ysmote, yada, ydro,
+				 ydecom, yeda, yemco, yemco2, ymco],
+				["Original", "ROS", "SMOTE", "ADASYN", "DRO",
+				 "DECOM", "EDA", "EMCO", "EMCO(0.1)", "MCO"]):
 			if name == "DRO":
-				### DRO transforms also the test data:
+				### DRO also transforms the test data:
 				res = test(X, y, Xdro_te, y_te, tol=svc_tol)
 			else:
 				res = test(X, y, X_te, y_te, tol=svc_tol)
+			
 			method.append(name)
 			bAcc.append(res[0])
 			TPR.append(res[1])
