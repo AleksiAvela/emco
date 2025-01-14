936737c57c1773a05fc296fdfd5d04bc02190db8
AleksiAvela <106800331+AleksiAvela@users.noreply.github.com>
2025-01-14 13:01:13 +0200

    Update example.py

--- src/example.py
+++ src/example.py
@@ -14,6 +14,7 @@ from imblearn.over_sampling import SMOTE
 from imblearn.over_sampling import ADASYN
 from pydro.src.dro import DistributionalRandomOversampling
 from DECOMlike import LDAOS
+from edaos import EDAOversampling as EDAOS
 from emco import ExtrapolatedMarkovChainOversampling as EMCO
 
 
@@ -53,8 +54,7 @@ assert min_f < 0.75*balance_ratio, "Select a category whose relative frequency i
 
 
 ### Initialize the training and test sets:
-tr_docs, tr_vocabulary, y_tr, te_docs, y_te = initialize_data(
-	   pos_class=category,
+tr_raw, tr_docs, tr_vocabulary, tr_labels, te_docs, te_labels = initialize_data(
 	   stopwords=stopwords,
 	   train_min_df=train_min_df,
 	   only_headlines=only_headlines)
@@ -71,6 +71,13 @@ pipe = Pipeline([('count', CountVectorizer(vocabulary=tr_vocabulary)),
 
 X_tr = dok_matrix(pipe.transform(tr_corpus))
 X_te = dok_matrix(pipe.transform(te_corpus))
+
+### Create binary target vectors from the raw labels:
+y_tr, y_te = [], []
+for train_label_set in tr_labels:
+	y_tr.append(int(category in train_label_set))
+for test_label_set in te_labels:
+	y_te.append(int(category in test_label_set))
 
 sampling_strategy = balance_ratio/(1-balance_ratio)
 
@@ -92,9 +99,12 @@ train_nwords = np.asarray(X_tr.sum(axis=1)).reshape(-1)
 test_nwords  = np.asarray(X_te.sum(axis=1)).reshape(-1)
 # These are the original DECOM hyperparameters:
 decom = LDAOS(n_topics=30, alpha=50/30, beta=0.01, max_iter=300)
-# MCO with gamma = 0 and EMCO with gamma = 1:
+# EDA Oversampling:
+edaos = EDAOS(s_ratio=balance_ratio)
+# MCO with gamma = 0 and EMCO with gamma = 1 and gamma = 0.1:
 mco   = EMCO(gamma=0)
 emco  = EMCO(gamma=1)
+emco2 = EMCO(gamma=0.1)
 
 ### ROS:
 Xros, yros = ros.fit_resample(X_tr, y_tr)
@@ -120,28 +130,41 @@ Xdecom, ydecom = decom.sample(s_ratio=balance_ratio)
 # Then the whole DECOM sample is transformed:
 Xdecom = pipe['tfidf'].transform(dok_matrix(Xdecom))
 
+### EDA Oversampling:
+eda_docs, yeda = edaos.sample(tr_raw, y_tr)
+Xeda = dok_matrix(pipe.transform(eda_docs))
+
 ### MCO and EMCO:
 mco.fit(tr_docs, y_tr, pos_class=1)
 emco.fit(tr_docs, y_tr, pos_class=1)
+emco2.fit(tr_docs, y_tr, pos_class=1)
 # First oversample the synthetic documents:
 mcodocs, ymco   = mco.sample(s_ratio=balance_ratio, length='auto', complete=True)
 emcodocs, yemco = emco.sample(s_ratio=balance_ratio, length='auto', complete=True)
+emcodocs2, yemco2 = emco2.sample(s_ratio=balance_ratio, length='auto', complete=True)
 # Then transform the oversampled data:
-Xmco  = dok_matrix(pipe.transform([' '.join(doc) for doc in mcodocs]))
-Xemco = dok_matrix(pipe.transform([' '.join(doc) for doc in emcodocs]))
+Xmco   = dok_matrix(pipe.transform([' '.join(doc) for doc in mcodocs]))
+Xemco  = dok_matrix(pipe.transform([' '.join(doc) for doc in emcodocs]))
+Xemco2 = dok_matrix(pipe.transform([' '.join(doc) for doc in emcodocs2]))
 
 print("\nTesting with linear SVM ...")
 print(datetime.now(), "\n")
 
 method,bAcc,TPR,TNR,prec,f_1,f_2 = [],[],[],[],[],[],[]
-for X, y, name in zip([X_tr, Xros, Xsmote, Xada, Xdro, Xdecom, Xemco, Xmco],
-		      [y_tr, yros, ysmote, yada, ydro, ydecom, yemco, ymco],
-		      ["Original","ROS","SMOTE","ADASYN","DRO","DECOM","EMCO","MCO"]):
+
+for X, y, name in zip(
+		[X_tr, Xros, Xsmote, Xada, Xdro,
+		 Xdecom, Xeda, Xemco, Xemco2, Xmco],
+		[y_tr, yros, ysmote, yada, ydro,
+		 ydecom, yeda, yemco, yemco2, ymco],
+		["Original", "ROS", "SMOTE", "ADASYN", "DRO",
+		 "DECOM", "EDA", "EMCO(1.0)", "EMCO(0.1)", "MCO"]):
 	if name == "DRO":
-		### DRO transforms also the test data:
+		### DRO also transforms the test data:
 		res = test(X, y, Xdro_te, y_te, tol=svc_tol)
 	else:
 		res = test(X, y, X_te, y_te, tol=svc_tol)
+
 	method.append(name)
 	bAcc.append(res[0])
 	TPR.append(res[1])
